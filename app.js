eddad5dca12da1ca15e07016edb6832999064ea2
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-08 13:01:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
-const moment = require("moment");
-const fs = require("fs/promises");
 const cors = require("cors");
-const socketIo = require("socket.io");
-const { createServer } = require("node:http");
-const path = require("path");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
-const io = socketIo(server);
+
+const io = new Server(server, {
+  cors: {
+    origin: "https://spa-application-orders-products.vercel.app",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 let activeSessions = 0;
 
@@ -26,23 +30,10 @@ io.on("connection", (socket) => {
 
 app.use(cors());
 
-app.use(async (req, res, next) => {
-  const { method, url } = req;
-  const data = moment().format("DD-MM-YYYY hh:mm:ss a");
-  await fs.appendFile("./public/server.log", `\n${method} ${url} ${data}`);
-  next();
-});
-
-app.use(express.static(path.join(__dirname, "build")));
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.send("Hello world");
 });
 
-app.use((req, res) => {
-  res.status(404).json({
-    message: "Not Found",
-  });
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
-
-server.listen(3000, () => console.log("Server running on port 3000"));
