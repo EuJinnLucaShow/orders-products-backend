dd25de0199451b76910546e0dfc255a507efb619
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-08 15:14:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const moment = require("moment");
-const fs = require("fs/promises");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
@@ -10,7 +8,6 @@ const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "https://spa-application-orders-products.vercel.app/",
-    methods: ["GET", "POST"],
     credentials: true,
   },
   transports: ["websocket", "polling"],
@@ -19,22 +16,13 @@ const io = new Server(server, {
 let activeSessions = 0;
 
 io.on("connection", (socket) => {
-  activeSessions++;
-  console.log("New client connected");
+  activeSessions++;  
   io.emit("sessionUpdate", activeSessions);
 
   socket.on("disconnect", () => {
-    activeSessions--;
-    console.log("Client disconnected");
+    activeSessions--;    
     io.emit("sessionUpdate", activeSessions);
   });
-});
-
-io.use(async (socket, next) => {
-  const { method, url } = socket.request;
-  const data = moment().format("DD-MM-YYYY hh:mm:ss a");
-  await fs.appendFile("./public/server.log", `\n${method} ${url} ${data}`);
-  next();
 });
 
 server.listen(3000, () => {
